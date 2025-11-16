a2ede1a92122ce4bd26df2d356dc8f50c631bb66
Estebansito1 <estebansito619@gmail.com>
2025-11-15 16:08:32 -0800

    update heap code

--- MIN_Heap.cpp
+++ MIN_Heap.cpp
@@ -17,5 +17,63 @@ using namespace std; // so i dont have to use std:: everytime
         while (index > 0) // stop when we reach the first elemet
             {
             int parent = (index - 1) / 2; //find the parent possition
+
+            if (heap[parent] > heap[index]) // if parent > child = violates heap min
+                {
+                    swap(heap[parent], heap[index]); // swap them
+                index = parent; // move up
+                } else break; // everything is good stop
             }
     }
+
+// move the new root down the tree after removal
+    void heapifyDown(vector<int>& heap, int index)
+    {
+        int size = heap.size();
+
+        while (true) // loop while conditon is true
+            {
+                int left = 2*index+1; //left child index
+                int right = 2*index+2; // right child index
+                int smallest=index; // assume the current index is smallest
+
+            // choose the smallest if it exists
+            if (left <size && heap[left] < heap[smallest]) smallest = left;
+            if (right < size && heap[right] <heap[smallest]) smallest = right;
+
+            if (smallest != index) //if parent is smaller than parent
+                {
+                    swap(heap[index], heap[smallest]); // swap downwards
+                index = smallest; // continue heapifying
+                }
+            else break; // no more vialations = everything is correct
+            }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
